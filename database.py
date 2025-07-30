86749c1b5f8136e20b52aaee05a598768f473de4
ND_Web <55359442+TheWolf95-WEB@users.noreply.github.com>
2025-07-30 22:19:54 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import sqlite3
 import json
 from pathlib import Path
+from datetime import datetime
 
 DB_PATH = Path("/opt/ndloadouts_storage/builds.db")
 DB_PATH.parent.mkdir(exist_ok=True)
@@ -52,7 +53,9 @@ def get_all_builds():
             "top2": row[4],
             "top3": row[5],
             "tabs": json.loads(row[6]) if row[6] else [],
-            "image": row[7]
+            "image": row[7],
+            "date": row[8] if len(row) > 8 else None
+
         })
     return builds
 
@@ -61,19 +64,18 @@ def add_build(data):
     conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
     c.execute("""
-        INSERT INTO builds (title, weapon_type, top1, top2, top3, tabs_json, image)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
-    """, (
-        data["title"],
-        data["weapon_type"],
-        data["top1"],
-        data["top2"],
-        data["top3"],
-        json.dumps(data["tabs"]),
-        data.get("image")
-    ))
-    conn.commit()
-    conn.close()
+    INSERT INTO builds (title, weapon_type, top1, top2, top3, tabs_json, image, date)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+""", (
+    data["title"],
+    data["weapon_type"],
+    data["top1"],
+    data["top2"],
+    data["top3"],
+    json.dumps(data["tabs"]),
+    data.get("image"),
+    data.get("date")
+))
 
 
 # Удаление сборки
@@ -106,5 +108,39 @@ def get_all_users():
     rows = c.fetchall()
     conn.close()
     return [{"id": row[0], "first_name": row[1], "username": row[2]} for row in rows]
+
+# Если дата еще не добавлену в сборку то обновляем
+def add_date_column_if_not_exists():
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+
+    # Проверяем наличие колонки
+    c.execute("PRAGMA table_info(builds)")
+    columns = [col[1] for col in c.fetchall()]
+    if "date" not in columns:
+        c.execute("ALTER TABLE builds ADD COLUMN date TEXT")
+        print("Поле date добавлено.")
+    else:
+        print("Поле date уже существует.")
+    
+    conn.commit()
+    conn.close()
+
+def fill_empty_dates():
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+    today = datetime.now().strftime('%Y-%m-%d')
+    c.execute("UPDATE builds SET date = ? WHERE date IS NULL OR date = ''", (today,))
+    conn.commit()
+    conn.close()
+    print("Обновлены пустые даты.")
+
+
+
+if __name__ == '__main__':
+    init_db()
+    add_date_column_if_not_exists()
+    fill_empty_dates()
+
 
 
