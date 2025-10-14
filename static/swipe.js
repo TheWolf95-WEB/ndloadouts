6be83bbccec8253a2d7911fd3678d20c445d0d73
theWolf <55359442+TheWolf95-WEB@users.noreply.github.com>
2025-10-14 10:05:39 +0300

    Update swipe.js

--- static/swipe.js
+++ static/swipe.js
@@ -1,1 +1,7 @@
-
+TGSwipeBack.init({
+  screenSelector: '.screen',
+  activeClass: 'active',
+  backHandler: (prevId) => showScreen(prevId),
+  easing: 'cubic-bezier(0.22, 0.61, 0.36, 1)',
+  threshold: 0.25,
+});
