f06186d3f0c9f18caec84305132ae603077ce53f
theWolf <55359442+TheWolf95-WEB@users.noreply.github.com>
2025-10-17 22:15:11 +0300

    Update import_bf.py

--- import_bf.py
+++ import_bf.py
@@ -6,7 +6,7 @@ from database_bf_settings import (
     add_bf_setting
 )
 
-JSON_PATH = Path("data/bf/campaign.json")
+JSON_PATH = Path("data/bf/controller.json")
 
 
 def normalize_type(t: str) -> str:
